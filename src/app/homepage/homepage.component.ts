f509bb06c968bec74cfe30bfe1f001745c2ce576
Nikita Negi <nikinegi1998@gmail.com>
2025-01-25 18:12:01 +0530

    Add login drawer and update sign-in button functionality

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,18 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { ImageModule } from 'primeng/image';
+import { DrawerModule } from 'primeng/drawer';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 @Component({
   selector: 'app-homepage',
-  imports: [ImageModule],
+  imports: [ImageModule ,DrawerModule, ButtonModule, InputTextModule],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent implements OnInit {
 
-  constructor(){}
+  visibleLogin: boolean = false;
 
-  ngOnInit(): void {
-    
+  constructor() { }
+
+  ngOnInit(): void {}
+
+  showLoginDialog() {
+    this.visibleLogin = true;
   }
 
   redirectTo(route:string){
